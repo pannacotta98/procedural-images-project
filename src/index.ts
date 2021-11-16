59a4cccaf5ba5bfe6dca5ab236b79b962866c2aa
Algot Sandahl <algot.sandahl@gmail.com>
2021-11-16 14:35:51 +0100

    Add three.js to project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,50 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// // Option 1: Import the entire three.js core library.
+import * as THREE from 'three';
 
-import confetti from 'canvas-confetti';
+// const scene = new THREE.Scene();
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// // Option 2: Import just the parts you need.
+// import { Scene } from 'three';
+// const scene = new Scene();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
+
+// /**
+//  * This file is just a silly example to show everything working in the browser.
+//  * When you're ready to start on your site, clear the file. Happy hacking!
+//  **/
+
+// import confetti from 'canvas-confetti';
+
+// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+//   resize: true,
+//   useWorker: true,
+// })({ particleCount: 200, spread: 200 });
