9f43b5bd14fae4103e4304a289c9b2dd26dc70ee
Algot Sandahl <algot.sandahl@gmail.com>
2021-11-21 11:54:48 +0100

    Add orbit controls

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { defaultConfig } from './config';
 import testFrag from './shaders/testFrag.glsl';
 import testVert from './shaders/testVert.glsl';
 import { addSlider } from './gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // // Option 2: Import just the parts you need.
 // import { Scene } from 'three';
@@ -36,40 +37,59 @@ const material = new THREE.ShaderMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+controls.enableDamping = true;
+controls.minDistance = 1;
+controls.maxDistance = 10;
 camera.position.z = 5;
+
+controls.update();
 
 const animate = function (time?: number) {
   // TODO Make sure to use time to calculate delta time
   requestAnimationFrame(animate);
 
-  cube.rotation.x += defaultConfig.test.speed;
-  cube.rotation.y += defaultConfig.test.speed;
+  // cube.rotation.x += defaultConfig.test.speed;
+  // cube.rotation.y += defaultConfig.test.speed;
 
-  camera.zoom = defaultConfig.test.zoom;
-  camera.updateProjectionMatrix();
+  // camera.zoom = defaultConfig.test.zoom;
+  // camera.updateProjectionMatrix();
+
+  // required if controls.enableDamping or controls.autoRotate are set to true
+  controls.update();
 
   renderer.render(scene, camera);
 };
 
 animate();
 
-addSlider({
-  parentId: 'camera-setting',
-  value: defaultConfig.test.speed,
-  label: 'Testing speed',
-  min: 0,
-  max: 0.1,
-  onChange: (newVal) => (defaultConfig.test.speed = newVal),
-});
+console.log(OrbitControls);
+window.addEventListener('resize', onWindowResize);
 
-addSlider({
-  parentId: 'camera-setting',
-  value: defaultConfig.test.zoom,
-  label: 'Zoom',
-  min: 0.5,
-  max: 5,
-  onChange: (newVal) => (defaultConfig.test.zoom = newVal),
-});
+// addSlider({
+//   parentId: 'camera-setting',
+//   value: defaultConfig.test.speed,
+//   label: 'Testing speed',
+//   min: 0,
+//   max: 0.1,
+//   onChange: (newVal) => (defaultConfig.test.speed = newVal),
+// });
+
+// addSlider({
+//   parentId: 'camera-setting',
+//   value: defaultConfig.test.zoom,
+//   label: 'Zoom',
+//   min: 0.5,
+//   max: 5,
+//   onChange: (newVal) => (defaultConfig.test.zoom = newVal),
+// });
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 // TODO
 // How does frustum culling work?
