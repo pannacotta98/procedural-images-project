36431e74b138e11e5de4e6caecc9d47dde1618d6
pannacotta98 <algot.sandahl@gmail.com>
2022-01-01 21:18:07 +0100

    Small change to stars shader to improve distribution uniformity

--- src/sky/starsFrag.glsl
+++ src/sky/starsFrag.glsl
@@ -7,12 +7,7 @@ in vec3 outPosition;
 
 void main() {
   vec3 result = vec3(0.0);
-
-  // float otherNoiseSample = 0.5 * snoise(0.07 * outPosition) + 0.5;
-  // result += 0.1 * otherNoiseSample * vec3(0.9, 0.1, 0.9);
-
-  float starNoiseSample = 0.5 * snoise(2.0 * outPosition) + 0.5;
+  float starNoiseSample = 0.5 * snoise(2.0 * 30.0 * normalize(outPosition)) + 0.5;
   result += vec3(0.8 * smoothstep(0.88, 0.99, starNoiseSample));
-
   gl_FragColor = vec4(result, 1.0);
 }
